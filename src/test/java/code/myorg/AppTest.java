03757188d1cf51bd71133d8af14e46c24c200077
Achyut <adeshpande@carbonite.com>
2019-06-13 15:46:44 +0530

    first commit

---
+++ src/test/java/code/myorg/AppTest.java
@@ -0,0 +1,35 @@
+package code.myorg;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTruetest()
+    {
+        assertTrue( true );
+    }
+    
+    @Test
+	public void test1() {
+    	App ob = new App();
+		assertEquals(8, ob.add(4, 4));
+	}
+    
+    @Test
+	public void test2() {
+    	App ob = new App();
+		assertEquals(2, ob.divide(10, 5));
+	}
+}
